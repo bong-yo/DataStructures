14921d8ad261480c967a021cdde54401e2b4fe9c
LorenzoBongiovanni <lorenzo.bongiovanni@amplyfi.com>
2021-02-05 10:45:56 +0000

    updated gitignore

--- Array/array_ds.cpp
+++
@@ -1,6 +0,0 @@
-class ArrayList {
-    int size;
-  public:
-    void set_values (int,int);
-    int size() {return size;}
-};
--- Array/array_ds.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-class Array:
-    def __init__(self, size: int = 10):
-        slef.size = size
