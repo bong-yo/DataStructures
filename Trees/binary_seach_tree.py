892699ecfc69b2dbc9b0d368e99d37ccb5d2fea5
LorenzoBongiovanni <lorenzo.bongiovanni@amplyfi.com>
2021-03-11 21:23:31 +0000

    added Morris tree traversal

--- Trees/binary_seach_tree.py
+++ Trees/binary_seach_tree.py
@@ -1,5 +1,6 @@
 import sys
 from utils import Paths
+from typing import List
 sys.path.append(Paths.QUEUE_DIR)
 from my_queue import Queue
 
@@ -18,63 +19,64 @@ class BinarySearchTree():
     All elements of right subtree are bigger than node.
     Same value go to RIGHT child (by pure choice).
     '''
-    def __init__(self, root: Node = None):
+    def __init__(self, values: List[int]):
         super(BinarySearchTree, self).__init__()
-        self.root = root
+        self.root = None
+        for v in values:
+            self.insert(v)
 
-    def insert(self, node: Node, current: Node = 'root'):
+    def insert(self, value: int) -> bool:
+        def _insert(node: Node, root: Node) -> bool:
+            if node.key < root.key:  # Descend tree to left child.
+                if root.left is None:
+                    root.left = node
+                    return True
+                else:
+                    _insert(node, root.left)
+            elif node.key > root.key:  # Descend tree to right child.
+                if root.right is None:
+                    root.right = node
+                    return True
+                else:
+                    _insert(node, root.right)
+            return False  # Value is already in the tree.
+
+        node = Node(value)
         if self.root is None:
             self.root = node
+            return True
+        return _insert(node, self.root)
 
-        elif current == "root":
-            self.insert(node, self.root)
+    def delete(self, key, root: Node = "root") -> None:
+        if root == "root":
+            root = self.root
 
-        elif current is None:  # End of tree reached -> save node as a leaf.
-            current = node
-
-        elif node.key < current.key:  # Descend tree to left child.
-            if current.left is None:
-                current.left = node
-            else:
-                self.insert(node, current.left)
-
-        elif node.key >= current.key:  # Descend tree to right child.
-            if current.right is None:
-                current.right = node
-            else:
-                self.insert(node, current.right)
-
-        else:
-            print("Something wrong happened!")
-
-    def delete(self, key, current: Node = "root") -> None:
-        if current == "root":
-            current = self.root
-
-        elif current is None:
+        elif root is None:
             return False
 
-        if key < current.key:
-            self.delete(key, current.left)
+        if key < root.key:
+            self.delete(key, root.left)
 
-        elif key > current.key:
-            self.delete(key, current.right)
+        elif key > root.key:
+            self.delete(key, root.right)
 
-        elif key == current.key:
+        elif key == root.key:
             # Get rightmost (biggest) child of the left (smallest) subtree.
-            rm_child, rm_parent = self.get_rightmost_child(current.left)
+            rm_child, rm_parent = self.get_rightmost_child(root.left)
             rm_parent.right = rm_child.left
-            current.key = rm_child.key
+            root.key = rm_child.key
             return True
 
     def get_leftmost_child(self, node: Node) -> Node:
-        while node.left is not None:
+        parent = None
+        while node.left:
             parent = node
             node = node.left
         return node, parent
 
     def get_rightmost_child(self, node: Node) -> Node:
-        while node.right is not None:
+        parent = None
+        while node.right:
             parent = node
             node = node.right
         return node, parent
@@ -130,5 +132,31 @@ class BinarySearchTree():
                 q.enqueue({"node": x["node"].right, "level": x["level"] + 1})
         return levels
 
+    def Morris_inorder(self):
+        """Morris algo. for tree traversal with O(1) space complexity."""
+        root = self.root
+        while root:
+            if not root.left:
+                yield root.key
+                root = root.right
+            else:
+                # Find root predecessor (i.e. rightmost child of root's left child)
+                pre = root.left
+                while pre.right and not pre.right == root:
+                    pre = pre.right
+                if not pre.right:  # If the link doesn't exist yet: make root the right child of its inorder predecessor.
+                    pre.right = root
+                    root = root.left  # Move root to left child.
+                else:  # The left subtree of root has been completely traversed already.
+                    yield root.key
+                    root = root.right
+
     def __str__(self):
         return "\n".join(str(level) for level in self.trasverse_levelorder())
+
+
+if __name__ == "__main__":
+    bst = BinarySearchTree([17, 3, 15, 10, 3245, 3, 2, 56, 24, 7, 234, 14, 2, 1, 6, 3])
+    print(bst)
+    print()
+    print([x for x in bst.Morris_inorder()])
